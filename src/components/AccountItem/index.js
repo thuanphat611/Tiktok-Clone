6d6411c93b0110d8540c646693f043e305632130
Thuận Phát <phat06112003@gmail.com>
2023-09-09 18:45:48 +0700

    code header #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import classNames from "classnames/bind";
+import styles from "./AccountItem.module.scss";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+
+const cx = classNames.bind(styles);
+const avatar = 'https://khoinguonsangtao.vn/wp-content/uploads/2022/07/avatar-tiktok-nam-chibi.jpg'
+
+function AccountItem() {
+  return (  
+    <div className={cx("wrapper")}>
+      <img className={cx("avatar")} src={avatar} alt="avatar" />
+      <div className={cx("info")}>
+        <span className={cx("name")}>
+          <h4>Nguyen Van A</h4>
+          <BsFillCheckCircleFill className={cx("check")} />
+        </span>
+        <span className={cx("username")}>@nguyenVanA</span>
+      </div>
+    </div>
+  );  
+}
+
+export default AccountItem; 
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,34 +1,65 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
 import images from "~/assets/images";
+import Tippy from "@tippyjs/react/headless";
+import { Wrapper as PopperWrapper } from "~/components/Popper";
+import AccountItem from "~/components/AccountItem";
+
+//icons
 import { BiSearch } from "react-icons/bi";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { MdCancel } from "react-icons/md";
 
+import { useState, useEffect } from "react";
+
 const cx = classNames.bind(styles);
 
 function Header() {
+  const [searchResults, setSearchResults] = useState([1]);
+
+  useEffect(() => {
+
+  },[])
+
   return (
     <header className={cx("wrapper")}>
       <div className={cx("content")}>
         <div className={cx("logo")}>
-          <img src={images.logo} alt="Tiktok"/>
+          <img src={images.logo} alt="Tiktok" />
         </div>
 
-        <div className={cx("search")}>
-          <input type="text" placeholder="Tìm kiếm" spellCheck={false} />
-          <button className={cx("clear")}>
-            <MdCancel />
-          </button>
-          <AiOutlineLoading3Quarters className={cx("loading")}/>
-          <button className={cx("search-button")}>
-            <BiSearch />
-          </button>
-        </div>
+        <Tippy
+          interactive
+          visible={searchResults.length > 0}
+          render={(attrs) => (
+            <div className={cx("search-results")} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                <h4 className={cx("search-title")}>
+                  Tài khoản 
+                </h4>
 
-        <div className={cx("actions")}>
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+              </PopperWrapper>
+            </div>
+          )}
+        >
+          <div className={cx("search")}>
+            <input type="text" placeholder="Tìm kiếm" spellCheck={false} />
+            <button className={cx("clear")}>
+              <MdCancel />
+            </button>
+            <AiOutlineLoading3Quarters className={cx("loading")} />
+            <button className={cx("search-button")}>
+              <BiSearch />
+            </button>
+          </div>
+        </Tippy>
 
-        </div>
+        <div className={cx("actions")}></div>
       </div>
     </header>
   );
