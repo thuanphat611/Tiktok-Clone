f8e7c1631494de7278463a287322d54989d63c2d
Thuận Phát <phat06112003@gmail.com>
2023-09-09 16:03:44 +0700

    code header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,9 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
+import images from "~/assets/images";
+import { BiSearch } from "react-icons/bi";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { MdCancel } from "react-icons/md";
 
 const cx = classNames.bind(styles);
 
@@ -7,7 +11,24 @@ function Header() {
   return (
     <header className={cx("wrapper")}>
       <div className={cx("content")}>
-        
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="Tiktok"/>
+        </div>
+
+        <div className={cx("search")}>
+          <input type="text" placeholder="Tìm kiếm" spellCheck={false} />
+          <button className={cx("clear")}>
+            <MdCancel />
+          </button>
+          <AiOutlineLoading3Quarters className={cx("loading")}/>
+          <button className={cx("search-button")}>
+            <BiSearch />
+          </button>
+        </div>
+
+        <div className={cx("actions")}>
+
+        </div>
       </div>
     </header>
   );
